7eb68ce18e506b4f3600fb5f469aa408dc6ee39f
Anh Mai <abmai91@gmail.com>
2018-06-12 21:26:35 -0700

    Expose pass-through of snapshotName argument for `matchSnapshot`

--- src/index.js
+++ src/index.js
@@ -78,7 +78,7 @@ type MockFunctionType<TArgs, TReturn> = () => $Call<
   ExtractArgsReturnType<TArgs, TReturn>,
   JestFnType
 >;
-type MatchSnapshotType = mixed => void;
+type MatchSnapshotType = (mixed, string) => void;
 type CallableAssertType = (
   assert: typeof assert & {matchSnapshot: MatchSnapshotType}
 ) => void | Promise<void>;
@@ -90,7 +90,7 @@ let mockFunction: MockFunctionType<*, *>, test: any;
 if (typeof it !== 'undefined') {
   // Surface snapshot testing
   // $FlowFixMe
-  assert.matchSnapshot = tree => expect(tree).toMatchSnapshot();
+  assert.matchSnapshot = (tree, snapshotName) => expect(tree).toMatchSnapshot(snapshotName);
 
   /* eslint-env node, jest */
   test = (description, callback, ...rest) =>
